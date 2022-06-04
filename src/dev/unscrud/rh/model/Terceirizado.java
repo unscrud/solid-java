78074754d3837b7925d79e928ad578210c719276
Daniel Carvalho <daniel.devop@gmail.com>
2022-06-04 12:50:44 -0300

    Criação da classe de terceirizados

---
+++ src/dev/unscrud/rh/model/Terceirizado.java
@@ -0,0 +1,20 @@
+package dev.unscrud.rh.model;
+
+import java.math.BigDecimal;
+
+public class Terceirizado extends Funcionario {
+  private String empresa;
+
+  public String getEmpresa() {
+    return empresa;
+  }
+
+  public void setEmpresa(String empresa) {
+    this.empresa = empresa;
+  }
+
+  public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+    super(nome, cpf, cargo, salario);
+  }
+
+}
