824a6a34cfd81dbb54f6c0f65229737b3e5ca1b5
Daniel Carvalho <daniel.devop@gmail.com>
2022-06-04 11:58:44 -0300

    A fazer

--- src/dev/unscrud/rh/service/ReajusteService.java
+++ src/dev/unscrud/rh/service/ReajusteService.java
@@ -9,6 +9,7 @@ import dev.unscrud.rh.ValidacaoException;
 import dev.unscrud.rh.model.Funcionario;
 
 public class ReajusteService {
+  // TODO: Simplificar o isolando regras de validação em classes distintas
   public void reajustarSalarioDo(Funcionario funcionario, BigDecimal aumento) {
     BigDecimal salarioAtual = funcionario.getSalario();
     BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
